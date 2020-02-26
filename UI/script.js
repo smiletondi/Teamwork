38d41b250b6060e61e3dcf8894f638f7fb7953e5
Ismael Tondi <smiletondi@gmail.com>
2020-02-26 09:08:29 +0100

    Add a click event listener to the delete button

--- UI/script.js
+++ UI/script.js
@@ -1,5 +1,6 @@
 const articleList= document.querySelector(".my-article-list");
 
+// Deleting 
 document.querySelectorAll(".btn-del").forEach( btn => {
     btn.addEventListener("click", () => {
         articleList.removeChild(btn.closest(".article"));
